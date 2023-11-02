e87320cb86ba2afdd1481bfea577d17cfd2830fb
Nathan <nate.wright@mail.utoronto.ca>
2023-11-02 17:15:19 -0400

    making fixes to helper functions

--- Emissions_VAR_helper.py
+++ Emissions_VAR_helper.py
@@ -65,3 +65,53 @@ def remove_with_grangers(df: pd.DataFrame, all_column_names, maxlag=1) -> pd.Dat
     new_df = df.drop(columns=cols_to_drop)
     print(new_df.head)
     return new_df
+
+
+# this function returns the optimal diff and lag value for a dataframe based on how close the model is to the testing data
+# currently only tests for zero, one and two diffs 
+# assumes training_sets is structed as [train_no_dif, train_one_dif, train_two_difs]
+def best_diff_and_lag_value(df, training_sets, test, train, max_lag_value):
+    min_mean = None 
+    min_results = None 
+    num_of_difs = 0 
+    best_dif_and_lag = [0, 0]
+    for ts in training_sets:
+        col_string = ""
+        if num_of_difs > 0: 
+            col_string = "_" + str(num_of_difs) + "d"
+        for i in range(1, max_lag_value):
+            model = VAR(ts)
+            model_fitted = model.fit(i)
+            forecast_input = ts.values[-i:]
+            fc = model_fitted.forecast(y=forecast_input, steps=len(df) - len(train))
+            df_forecast = pd.DataFrame(fc, index=df.index[-(len(df) - len(train)):], columns=df.columns + col_string)
+            df_results = df_forecast
+            if num_of_difs == 1: 
+                df_results = invert_transformation(train, df_forecast, second_diff=False)
+            elif num_of_difs == 2: 
+                df_results = invert_transformation(train, df_forecast, second_diff=True)
+            
+            # rename to forecast if it has not been changed by inverting the transformation 
+            if num_of_difs == 0: 
+                df_results = df_results.rename(columns={"Total_emissions": "Total_emissions_forecast"})
+            # get difference between forecasted and actual values for Total_emissions 
+            print(df_results['Total_emissions_forecast'])
+            # calculate difference between test and forecast 
+            differences = df_results['Total_emissions_forecast'].sub(test['Total_emissions'])
+            differences = differences.abs()
+            print(differences)
+            # then average all the differences to find the average difference 
+            mean = differences.mean()
+
+            print("NUMBER OF DIFS: " + str(num_of_difs))
+            print("NUMBER OF LAGS: " + str(i))
+            print("MEAN: " + str(mean))
+            if min_mean is None or abs(mean) < min_mean: 
+                best_dif_and_lag = [num_of_difs, i]
+                min_mean = abs(mean) 
+                min_results = df_results 
+
+        num_of_difs += 1
+        max_lag_value -= 1
+
+    return best_dif_and_lag
